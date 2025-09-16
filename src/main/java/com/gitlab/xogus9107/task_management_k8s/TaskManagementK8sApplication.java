d8e66cf5d6c0430b2420a57110cba779ee97da5e
kth <xogus9107@gmail.com>
2025-09-17 01:14:25 +0900

    init

---
+++ src/main/java/com/gitlab/xogus9107/task_management_k8s/TaskManagementK8sApplication.java
@@ -0,0 +1,13 @@
+package com.gitlab.xogus9107.task_management_k8s;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskManagementK8sApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskManagementK8sApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gitlab/xogus9107/task_management_k8s/TaskManagementK8sApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gitlab.xogus9107.task_management_k8s;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagementK8sApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
